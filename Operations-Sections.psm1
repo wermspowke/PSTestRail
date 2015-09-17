8030d7d76ddecf9f636b728e1dceed8b6b326f73
balderslade <brendan.alderslade@landmark.co.uk>
2015-09-17 17:19:34 +0100

    Added get_section, add_section, update_section, delete_section as Get-, Add-, Set, Remove-

--- Operations-Sections.psm1
+++ Operations-Sections.psm1
@@ -1,5 +1,25 @@
-﻿function Get-TestRailSections
+﻿function Get-TestRailSection
 {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [int]
+        $SectionId
+    )
+
+    PROCESS
+    {
+        $Uri = "get_section/$SectionId"
+        $Parameters = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
+
+        Request-TestRailUri -Uri $Uri -Parameters $Parameters
+    }
+}
+
+function Get-TestRailSections
+{
+    [CmdletBinding()]
     param
     (
         [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
@@ -22,6 +42,123 @@
         {
             Add-UriParameters -Parameters $Parameters -Hash @{ suite_id = $SuiteId } 
         }
+
+        Request-TestRailUri -Uri $Uri -Parameters $Parameters
+    }
+}
+
+function Add-TestRailSection
+{
+    [CmdletBinding()]
+    param
+    (
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Name,
+        
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Alias('project_id')]
+        [int]
+        $ProjectId,
+
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [Alias('suite_id')]
+        [int]
+        $SuiteId,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Description,
+
+        [Parameter(Mandatory=$false)]
+        [int]
+        $ParentId
+    )
+
+    PROCESS
+    {
+        $Uri = "add_section/$ProjectId"
+        $Parameters = @{}
+
+        $Parameters.name = $Name
+
+        if ( $PSBoundParameters.ContainsKey("SuiteId") )
+        {
+            $Parameters.suite_id = $SuiteId
+        }
+
+        if ( $PSBoundParameters.ContainsKey("Description") )
+        {
+            $Parameters.description = $Description
+        }
+
+        if ( $PSBoundParameters.ContainsKey("ParentId") )
+        {
+            $Parameters.parent_id = $ParentId
+        }
+
+        Submit-TestRailUri -Uri $Uri -Parameters $Parameters
+    }
+}
+
+function Set-TestRailSection
+{
+    [CmdletBinding()]
+    param
+    (
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Alias('id')]
+        [int]
+        $SectionId,
+        
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Name,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Description
+    )
+
+    PROCESS
+    {
+        $Uri = "update_section/$SectionId"
+        $Parameters = @{}
+
+        if ( $PSBoundParameters.ContainsKey("Name") )
+        {
+            $Parameters.name = $Name
+        }
+
+        if ( $PSBoundParameters.ContainsKey("Description") )
+        {
+            $Parameters.description = $Description
+        }
+
+        Submit-TestRailUri -Uri $Uri -Parameters $Parameters
+    }
+}
+
+function Remove-TestRailSection
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [int]
+        $SectionId
+    )
+
+    PROCESS
+    {
+        $Uri = "delete_section/$SectionId"
+        $Parameters = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
 
         Request-TestRailUri -Uri $Uri -Parameters $Parameters
     }
